cf28b54ba1e3ef6df6562e54b418a63acc76f8af
Fraawdrinn <chatoui.wael40@gmail.com>
2025-02-01 00:33:39 +0100

    fixed issues

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-6xl font-bold text-[#9b4819]">404</h1>
       <p className="text-xl mt-4 text-[#a3503a]">
-        Oops! The page you're looking for doesn't exist.
+        Oops! The page you&apos;re looking for doesn&apos;t exist.
       </p>
       <Link 
       href="/" 
