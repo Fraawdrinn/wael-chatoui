ec9c8c88dd21f0e4fbe56270c64dec4d9dc4cc73
Fraawdrinn <chatoui.wael40@gmail.com>
2025-03-27 10:05:23 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import Presentation from "@/components/presentation";
 import Footer from "@/components/footer";
 import Main from "@/components/main";
@@ -7,9 +5,7 @@ import Main from "@/components/main";
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      
 
       <div className="flex flex-col min-h-screen">
         <Presentation />
